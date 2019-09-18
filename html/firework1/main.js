bd8d7274d97d8f2fa9df6fc614a46c7df730a86c
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-09-18 17:13:44 +0200

    second commit

--- html/firework1/main.js
+++ html/firework1/main.js
@@ -2,3 +2,20 @@ const ctx = document.querySelector("canvas").getContext("2d");
 
 ctx.canvas.width = document.documentElement.clientWidth;
 ctx.canvas.height = document.documentElement.clientHeight;
+
+let firework = new Firework(getRandom(ctx.canvas.width), ctx.canvas.height);
+let gravity = new Vector2d(0, 0.08);
+
+function draw(){
+    ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+    firework.applyForce(gravity);
+    firework.update();
+    firework.show(ctx);
+    requestAnimationFrame(draw);
+}
+
+function getRandom(max){
+    return Math.floor(Math.random() * max);
+}
+
+draw();
